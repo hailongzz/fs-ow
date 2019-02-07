39e22c116aca40051d2431d8682979922b7a6086
zhanghailong <512427121@qq.com>
2019-02-07 17:26:45 +0800

    first test

---
+++ src/FsOw.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="FsOw">
+    <Index/>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index.vue'
+
+export default {
+  name: 'FsOw',
+  components: {
+    Index
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Index msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Index from '@/components/Index.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Index
+  }
+}
+</script>
